ef36c9836520327cca2b5da509bfa057b2b04c58
codedis213 <codedis213@gmail.com>
2015-07-09 17:18:40 +0530

    Round-robin dispatching

--- send.py
+++ send.py
@@ -7,9 +7,14 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
+import sys
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='Hello World!')
+                      body=message)
+
 print " [x] Sent 'Hello World!'"
 
 connection.close()
